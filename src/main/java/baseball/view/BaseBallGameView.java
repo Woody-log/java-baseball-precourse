b6b147659c295a57925851d7e094504ad9db7588
Woody <yuncheol92@naver.com>
2022-04-16 23:37:55 +0900

    style: 게임 종료 띄어쓰기 수정

--- src/main/java/baseball/view/BaseBallGameView.java
+++ src/main/java/baseball/view/BaseBallGameView.java
@@ -8,7 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseBallGameView implements View {
 
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String INPUT_COMMAND = "게임을 새로 시작혀려면 1, 종료하려면 2를 입력하세요.";
     private final StringBuilder stringBuilder = new StringBuilder();
 
