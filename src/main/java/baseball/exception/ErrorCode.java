55ac4eac5f6b2126f065dfc0aff423d75abb7b21
Woody <yuncheol92@naver.com>
2022-04-16 17:05:24 +0900

    feat: 재시작과 종료 명령어 입력 기능

--- src/main/java/baseball/exception/ErrorCode.java
+++ src/main/java/baseball/exception/ErrorCode.java
@@ -1,11 +1,14 @@
 package baseball.exception;
 
 public enum ErrorCode {
-
+    // number 관련 에러
     INVALID_INPUT_STRING_NULL("입력값이 null 입니다."),
     ONLY_TREE_DIGITS("세 자리만 허용합니다."),
     EXIST_DUPLICATE_STRING("중복된 문자가 존재합니다."),
-    ONLY_NUMBERS_ALLOW("숫자만 입력 가능합니다.");
+    ONLY_NUMBERS_ALLOW("숫자만 입력 가능합니다."),
+
+    // command 관련 에러
+    INVALID_COMMAND("잘못된 명령어입니다.");
 
     private final String errorMessage;
 
--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -1,10 +1,15 @@
 package baseball.view;
 
 import baseball.model.ResultDto;
+import baseball.view.type.Command;
 
 public interface View {
 
     String inputString();
 
     void showResult(final ResultDto resultDto);
+
+    void showEnding();
+
+    Command inputCommand();
 }
