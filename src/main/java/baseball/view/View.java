58fa2cddf0f048a670abba4e1cd0698117e01232
Woody <yuncheol92@naver.com>
2022-04-16 21:19:48 +0900

    Revert "feat: 재시작과 종료 명령어 입력 기능"
    This reverts commit 55ac4eac5f6b2126f065dfc0aff423d75abb7b21.

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -1,15 +1,10 @@
 package baseball.view;
 
 import baseball.model.ResultDto;
-import baseball.view.type.Command;
 
 public interface View {
 
     String inputString();
 
     void showResult(final ResultDto resultDto);
-
-    void showEnding();
-
-    Command inputCommand();
 }
